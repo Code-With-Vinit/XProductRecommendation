7cc9b646e3e33ee783504a2d715ceaa33f3c4471
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-12-27 22:11:32 +0530

    Implemented the Chat Section UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import Home from './components/Home';
+import History from './components/History'
 
 function App() {
 
   return (
     <>
-     <h1>XProductRecommendation</h1>
+     <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/history' element={<History/>}/>
+        </Routes>
+     </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import BotLogo from '../assets/Bot_logo.png'
+import './Home.css'
+
+function Home() {
+  return (
+
+    <div className='layout'>
+
+    <div className='sidebar'>
+
+    </div>
+
+    <div className='container'>
+      <header>
+        <h1>Product Recommendation AI</h1>
+        <div>
+          <span>Light</span>
+          <img src="" alt="" />
+        </div>
+      </header>
+
+      <div className='requirement'>
+        <h1>Hi, Please tell me what you want?</h1>
+        <img src={BotLogo} alt="BotLogo" className='BotLogo'/>
+      </div>
+
+      <div className='OptionGrid'>
+        <div className='OptionCard'>
+          <h1>Jeans</h1>
+          <p>Get immediate AI generated response</p>
+        </div>
+
+        <div className='OptionCard'>
+          <h1>SmartPhone</h1>
+          <p>Get immediate AI generated response</p>
+        </div>
+
+        <div className='OptionCard'>
+          <h1>Laptop</h1>
+          <p>Get immediate AI generated response</p>
+        </div>
+
+        <div className='OptionCard'>
+          <h1>T-Shirt</h1>
+          <p>Get immediate AI generated response</p>
+        </div>
+      </div>
+
+      <div className='Input'>
+        <input type="text" placeholder='Please tell me about your query!' />
+        <button className='btn'>Ask</button>
+        <button className='btn'>Save</button>
+      </div>
+    
+    </div>
+    </div>
+  )
+}
+
+export default Home
