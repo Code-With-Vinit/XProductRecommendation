375754d1663d2e95b195a58a9941d179657233df
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2026-01-23 23:58:14 +0530

    Debugging

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
     <div className='container'>
       <header>
         <h1>Product Recommendation AI</h1>
-        console.log("Debugging");
+        console.log("debugging");
         <div className='mode-switch'>
           <span>{theme==="dark"? "Dark":"Light"}</span>
           <label htmlFor="theme-toggle">
